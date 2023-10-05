d9ece006fd9a7ceedfa46fbbe8206dd1f31fec84
sean-madu <madusean2@gmail.com>
2023-10-04 22:33:40 -0600

    Init Commit

--- httpclient.py
+++ httpclient.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 # coding: utf-8
 # Copyright 2016 Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,16 +24,22 @@ import re
 # you may use urllib to encode data appropriately
 import urllib.parse
 
+
+PORT = 80
+
+
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
+
 
 class HTTPResponse(object):
     def __init__(self, code=200, body=""):
         self.code = code
         self.body = body
 
+
 class HTTPClient(object):
-    #def get_host_port(self,url):
+    # def get_host_port(self,url):
 
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -43,15 +49,15 @@ class HTTPClient(object):
     def get_code(self, data):
         return None
 
-    def get_headers(self,data):
+    def get_headers(self, data):
         return None
 
     def get_body(self, data):
         return None
-    
+
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
-        
+
     def close(self):
         self.socket.close()
 
@@ -68,8 +74,22 @@ class HTTPClient(object):
         return buffer.decode('utf-8')
 
     def GET(self, url, args=None):
+        print("GEtting URL ")
+        print(url)
+
         code = 500
-        body = ""
+        body = f'GET / HTTP/1.0\r\nHost: {url}\r\n\r\n'
+        self.connect(url, PORT)  # Cant find 127 urls?
+        try:
+            self.sendall(body)
+        except socket.error:
+            print('Could not send GET request')
+            print(socket.error)
+            sys.exit()
+
+        self.socket.shutdown(socket.SHUT_WR)
+        buffer = self.recvall(self.socket)
+        print(buffer)
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
@@ -79,17 +99,20 @@ class HTTPClient(object):
 
     def command(self, url, command="GET", args=None):
         if (command == "POST"):
-            return self.POST( url, args )
+            return self.POST(url, args)
         else:
-            return self.GET( url, args )
-    
+            return self.GET(url, args)
+
+
 if __name__ == "__main__":
+    print(sys.argv)
     client = HTTPClient()
     command = "GET"
     if (len(sys.argv) <= 1):
         help()
         sys.exit(1)
     elif (len(sys.argv) == 3):
-        print(client.command( sys.argv[2], sys.argv[1] ))
+        print(client.command(sys.argv[2], sys.argv[1]))
+
     else:
-        print(client.command( sys.argv[1] ))
+        print(client.command(sys.argv[1]))
